d8da1d443521886fd316b4b95f41e47d4c98a962
Aman Arya <aman.arya524@gmail.com>
2017-07-07 01:12:55 -0400

    Initial commit

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#mainly to debug locally
+
+if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
+#ENV="IUHPC"
+
+#clean up previous job (just in case)
+rm -f finished
+
+if [ $ENV == "IUHPC" ]; then
+	jobid=`qsub $SERVICE_DIR/submit.pbs`
+	#jobid=`qsub -q preempt $SERVICE_DIR/submit.pbs`
+	echo $jobid > jobid
+fi
+
+if [ $ENV == "VM" ]; then
+	nohup time $SERVICE_DIR/submit.pbs > stdout.log 2> stderr.log &
+	echo $! > pid
+fi
---
+++ stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#if [ -f finished ]; then
+#    echo "can't stop job that's already finished"
+#    exit 1
+#fi
+
+#ENV="IUHPC"
+
+if [ $ENV == "IUHPC" ]; then
+    jobid=`cat jobid`
+    echo "running qdel $jobid"
+    qdel $jobid
+fi
+
+if [ $ENV == "VM" ]; then
+    pid=`cat pid`
+    echo "running kill" 
+    kill $pid
+fi
