13dd99145e6eed9b0cc05dc302e7cf1469000ced
Aman Arya <aman.arya524@gmail.com>
2017-07-07 13:04:34 -0400

    Add tck2trk support

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dipy.workflows.align import whole_brain_slr_flow
 from dipy.workflows.segment import recognize_bundles_flow
 #from dipy.workflows.viz import horizon_flow
 import json
+import nibabel as nib
 
 
 if __name__ == '__main__':
@@ -10,15 +11,25 @@ if __name__ == '__main__':
     with open('config.json') as config_json:
         config = json.load(config_json)
 
-    wtrk = config['moving_trk']
-    etrk = config['static_trk']
+    wtrk = config['moving_tck']
+    etrk = config['static_tck']
+    wtrk = str(wtrk)
+    etrk = str(etrk)
+
     path = os.getcwd()
+    w_file = nib.streamlines.load(wtrk)
+    nib.streamlines.save(w_file.tractogram, path+'/mv.trk')
+
+    e_file = nib.streamlines.load(etrk)
+    nib.streamlines.save(e_file.tractogram, path+'/st.trk')
+
     bundles_flow = path+'/bundles_flow/'
     if not os.path.exists(bundles_flow):
         os.makedirs(bundles_flow)
     slr_flow = path+'/slr_flow/'
     if not os.path.exists(slr_flow):
         os.makedirs(slr_flow)
-    whole_brain_slr_flow(moving_streamlines_files=wtrk, static_streamlines_file=etrk, out_dir=slr_flow, verbose=True)
+
+    whole_brain_slr_flow(moving_streamlines_files=path+'/mv.trk', static_streamlines_file=path+'/st.trk', out_dir=slr_flow, verbose=True)
     recognize_bundles_flow(streamline_files=etrk, model_bundle_files=wtrk,out_dir=bundles_flow,verbose=True)
     #horizon_flow(input_files=bundles_flow)s
