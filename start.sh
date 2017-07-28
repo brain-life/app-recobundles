6499332044bfadb1fd58020c6c5e587dec99136e
Aman Arya <aman.arya524@gmail.com>
2017-07-28 18:05:03 -0400

    Add "

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ if [ $ENV == "SINGULARITY" ]; then
 cat <<EOT > _run.sh
 time singularity run /usr/local/images/brainlife_recobundles.img
 #check for output files
-count=$(ls bundles_flow/*.trk | wc -l)
-if [ $count -eq 2 ];
+count=\$(ls bundles_flow/*.trk | wc -l)
+if [ \$count -eq 2 ];
 then
     echo 0 > finished
 else
