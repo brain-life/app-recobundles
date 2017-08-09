82c2585472044dc57c2feea4c1e046bed3349d87
Soichi Hayashi <hayashis@iu.edu>
2017-08-09 09:43:06 -0400

    Update status.sh
    added check for missing pid in status.sh

--- status.sh
+++ status.sh
@@ -50,8 +50,14 @@ if [ -f jobid ]; then
 fi
 
 if [ -f pid ]; then
-    echo "assume to be running locally"
-    exit 0
+    if ps -p $(cat pid) > /dev/null
+    then
+	    tail -1 stdout.log
+	    exit 0
+    else
+	    echo "no longer running but didn't finish"
+	    exit 2
+    fi
 fi
 
 echo "can't determine the status - maybe not yet started?"
